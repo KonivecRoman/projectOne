8e64f133875891f971dc70d84df9aabb5f51799c
Roman Konivec <r.konivec@aimprosoft.com>
2020-12-05 13:03:46 +0200

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
+        System.out.println("125");
     }
 }
